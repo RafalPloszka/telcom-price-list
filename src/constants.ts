b1d98dbc2e7af917b5aeaf92b70fa212ca10d05b
RafalPloszka <rafalploszka@live.com>
2023-08-21 11:13:47 +0200

    feat: add texts as an object

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const TEXTS = {
+  title: "Kalkulator usług",
+  pickYear: "Wybierz rok",
+  pickProducts: "Wybierz usługi",
+  productsRequired: "Wybierz przynajmniej jedną usługę",
+  submitButtonLabel: "Oblicz",
+  total: "Cena regularna",
+  totalWithDiscount: "Cena promocyjna",
+};
